ebfbff4ee50dd75ad26341e643b37174470f82e0
Sander Tonheim <tonheims98@gmail.com>
2025-10-27 13:12:47 +0100

    updated password

--- database/database.js
+++ database/database.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2/promise");
 
 async function createConnection() {
-    return (connection = await mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "DB123",
-        database: "cars",
-    }));
+	return (connection = await mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "Kappa123",
+		database: "cars",
+	}));
 }
 
 module.exports = { createConnection };
