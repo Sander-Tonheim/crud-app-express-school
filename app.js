26df833231a827024fe448552e2ae9d01cf6a647
Sander Tonheim <tonheims98@gmail.com>
2025-10-27 13:19:21 +0100

    added comments

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+// Importerer pakker
 const express = require("express");
-const app = express();
-const port = 3000;
 const mysql = require("mysql2/promise");
 const { createConnection } = require("./database/database.js");
-
+// Definerer port og express serveren
+const app = express();
+const port = 3000;
+// Forteller express at malmotor skal være ejs
 app.set("view engine", "ejs");
+// Definerer rute for CSS filer
 app.use(express.static("public"));
 
+// Rute til hjemmeside
 app.get("/", async (req, res) => {
+	// Lager kobler til databasen
 	const connection = await createConnection();
+	// Henter data fra databasen
 	const [results] = await connection.query("SELECT * FROM car");
+	// Definerer responsen på denne ruten, spesifiserer mal og sender data til mal.
 	res.render("index", { cars: results });
 });
 
@@ -18,7 +25,12 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/bruker", (req, res) => {
-	res.render("bruker", { names: ["per", "Ole", "Olesya", "Ådne"], fact: true, req: req.query });
+	res.render("bruker", {
+		names: ["per", "Ole", "Olesya", "Ådne"],
+		fact: true,
+		// Henter URL paramter.
+		req: req.query,
+	});
 });
 
 app.listen(port, () => {
